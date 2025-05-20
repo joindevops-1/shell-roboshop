760709a51ccea2936749f03389e5d0e380b243c1
msivakumarreddy <info@joindevops.com>
2025-05-20 13:06:53 +0530

    hello world started

--- payment.sh
+++ payment.sh
@@ -52,6 +52,8 @@ rm -rf /app/*
 cd /app 
 unzip /tmp/payment.zip &>>$LOG_FILE
 VALIDATE $? "Downloaded payment app"
+pip3 install -r requirements.txt
+
 
 cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload  &>>$LOG_FILE
