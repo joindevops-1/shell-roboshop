7cdd96a257a742aeecf81f821fbddb14a6afa1e4
msivakumarreddy <info@joindevops.com>
2025-05-21 10:41:37 +0530

    hello world started

--- mysql.sh
+++ mysql.sh
@@ -1,36 +1,9 @@
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+#!/bin/bash
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+source ./common.sh
+app_name=mysql
 
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
-
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+check_root
 
 dnf install mysql-server -y
 systemctl enable mysqld
@@ -40,3 +13,4 @@ VALIDATE $? "Installed and Started MySQL"
 
 mysql_secure_installation --set-root-pass RoboShop@1
 VALIDATE $? "Root password setup"
+print_time
